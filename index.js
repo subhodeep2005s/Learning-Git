90bc775516c59e50e5bc241e38dde760c8dad79f
TonyStark <sarkarsubhodeep417@gmail.com>
2025-01-05 20:12:01 +0530

    end

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
-const age=24;
-console.log(age);
-confirm('Please enter your age');   
+// const age=24;
+// console.log(age);
+// confirm('Please enter your age');   
 
-const name=prompt('Please enter your name');
-console.log(name);
-function greet(name){
-    console.log('Hello '+name);
-};
-function newFnn(){
-    console.log('This is a new function');
+// const name=prompt('Please enter your name');
+// console.log(name);
+// function greet(name){
+//     console.log('Hello '+name);
+// };
+// function newFnn(){
+//     console.log('This is a new function');
     
+// }
+
+const pass="736146"
+
+const arr=[0,1,2,3,4,5,6,7,8,9]
+var res=""
+for (i=0;i<arr.length;i++)
+{
+    for(j=0;j<arr.length;j++){
+        res=i+""+j
+        if (res==pass) console.log("cracked!")
+            break
+    }
+    res=""
 }
--- script.js
+++ script.js
@@ -2,6 +2,6 @@ function greet(name,age){
     console.log('Hello '+name+' you are '+age+' years old'+"\n");
 };
 
-function isAvobe18(){
+function isAvobe_18(){
     return age>18;
 }
